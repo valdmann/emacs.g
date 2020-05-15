5a220fa1bee5a33a5e72b8496d6820b25e025af5
Jüri Valdmann <juri.valdmann@gmail.com>
2020-05-16 00:53:33 +0200

    Remove use-package

--- init.el
+++ init.el
@@ -1,206 +1,171 @@
 ;;; init.el --- user-init-file                    -*- lexical-binding: t -*-
-;;; Early birds
-(progn ;     startup
-  (defvar before-user-init-time (current-time)
-    "Value of `current-time' when Emacs begins loading `user-init-file'.")
-  (message "Loading Emacs...done (%.3fs)"
-           (float-time (time-subtract before-user-init-time
-                                      before-init-time)))
-  (setq user-init-file (or load-file-name buffer-file-name))
-  (setq user-emacs-directory (file-name-directory user-init-file))
-  (message "Loading %s..." user-init-file)
-  (when (< emacs-major-version 27)
-    (setq package-enable-at-startup nil)
-    ;; (package-initialize)
-    (load-file (expand-file-name "early-init.el" user-emacs-directory)))
-  (setq inhibit-startup-buffer-menu t)
-  (setq inhibit-startup-screen t)
-  (setq inhibit-startup-echo-area-message "locutus")
-  (setq initial-buffer-choice t)
-  (setq initial-scratch-message "")
-  (setq load-prefer-newer t)
-  (scroll-bar-mode 0)
-  (tool-bar-mode 0)
-  (menu-bar-mode 0))
 
-(progn ;    `borg'
-  (add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
-  (require  'borg)
-  (borg-initialize))
+(defvar before-user-init-time (current-time)
+  "Value of `current-time' when Emacs begins loading `user-init-file'.")
+(message "Loading Emacs...done (%.3fs)"
+         (float-time (time-subtract before-user-init-time
+                                    before-init-time)))
+(message "Loading %s..." user-init-file)
 
-(progn ;    `use-package'
-  (require  'use-package)
-  (setq use-package-verbose t))
+(setq inhibit-startup-buffer-menu t)
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-echo-area-message t)
+(setq initial-scratch-message "")
+(setq load-prefer-newer t)
+(scroll-bar-mode 0)
+(tool-bar-mode 0)
+(menu-bar-mode 0)
 
-(use-package auto-compile
-  :demand t
-  :config
-  (auto-compile-on-load-mode)
-  (auto-compile-on-save-mode)
-  (setq auto-compile-display-buffer               nil)
-  (setq auto-compile-mode-line-counter            t)
-  (setq auto-compile-source-recreate-deletes-dest t)
-  (setq auto-compile-toggle-deletes-nonlib-dest   t)
-  (setq auto-compile-update-autoloads             t))
+(add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
+(require  'borg)
+(borg-initialize)
 
-(use-package epkg
-  :defer t
-  :init (setq epkg-repository
-              (expand-file-name "var/epkgs/" user-emacs-directory)))
+(require 'auto-compile)
+(auto-compile-on-load-mode)
+(auto-compile-on-save-mode)
+(setq auto-compile-display-buffer               nil)
+(setq auto-compile-mode-line-counter            t)
+(setq auto-compile-source-recreate-deletes-dest t)
+(setq auto-compile-toggle-deletes-nonlib-dest   t)
+(setq auto-compile-update-autoloads             t)
 
-(use-package custom
-  :no-require t
-  :config
-  (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-  (when (file-exists-p custom-file)
-    (load custom-file)))
+(require 'epkg)
+(setq epkg-repository
+      (expand-file-name "var/epkgs/" user-emacs-directory))
 
-(use-package server
-  :config (or (server-running-p) (server-mode)))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
-(progn ;     startup
-  (message "Loading early birds...done (%.3fs)"
-           (float-time (time-subtract (current-time)
-                                      before-user-init-time))))
+(require 'server)
+(or (server-running-p) (server-mode))
 
-;;; Long tail
+(require 'dash)
+(dash-enable-font-lock)
 
-(use-package dash
-  :config (dash-enable-font-lock))
+(require 'diff-hl)
+(setq diff-hl-draw-borders nil)
+(global-diff-hl-mode)
+(add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh t)
 
-(use-package diff-hl
-  :config
-  (setq diff-hl-draw-borders nil)
-  (global-diff-hl-mode)
-  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh t))
+(require 'diff-mode)
+(when (>= emacs-major-version 27)
+  (set-face-attribute 'diff-refine-changed nil :extend t)
+  (set-face-attribute 'diff-refine-removed nil :extend t)
+  (set-face-attribute 'diff-refine-added   nil :extend t))
 
-(use-package diff-mode
-  :defer t
-  :config
-  (when (>= emacs-major-version 27)
-    (set-face-attribute 'diff-refine-changed nil :extend t)
-    (set-face-attribute 'diff-refine-removed nil :extend t)
-    (set-face-attribute 'diff-refine-added   nil :extend t)))
+(require 'eldoc)
+(global-eldoc-mode)
 
-(use-package dired
-  :defer t
-  :config (setq dired-listing-switches "-alh"))
+(require 'dired)
+(setq dired-listing-switches "-alh")
 
-(use-package eldoc
-  :when (version< "25" emacs-version)
-  :config (global-eldoc-mode))
+(require 'help)
+(temp-buffer-resize-mode)
 
-(use-package help
-  :defer t
-  :config (temp-buffer-resize-mode))
+(require 'lisp-mode)
+(add-hook 'emacs-lisp-mode-hook 'outline-minor-mode)
+(add-hook 'emacs-lisp-mode-hook 'reveal-mode)
+(defun indent-spaces-mode ()
+  (setq indent-tabs-mode nil))
+(add-hook 'lisp-interaction-mode-hook #'indent-spaces-mode)
 
-(progn ;    `isearch'
-  (setq isearch-allow-scroll t))
+(require 'magit)
+(define-key global-map (kbd "C-x g") 'magit-status)
+(define-key global-map (kbd "C-x M-g") 'magit-dispatch)
+(magit-add-section-hook 'magit-status-sections-hook
+                        'magit-insert-modules
+                        'magit-insert-stashes
+                        'append)
 
-(use-package lisp-mode
-  :config
-  (add-hook 'emacs-lisp-mode-hook 'outline-minor-mode)
-  (add-hook 'emacs-lisp-mode-hook 'reveal-mode)
-  (defun indent-spaces-mode ()
-    (setq indent-tabs-mode nil))
-  (add-hook 'lisp-interaction-mode-hook #'indent-spaces-mode))
+(require 'man)
+(setq Man-width 80)
 
-(use-package magit
-  :defer t
-  :bind (("C-x g"   . magit-status)
-         ("C-x M-g" . magit-dispatch))
-  :config
-  (magit-add-section-hook 'magit-status-sections-hook
-                          'magit-insert-modules
-                          'magit-insert-stashes
-                          'append))
+(require 'paren)
+(show-paren-mode)
 
-(use-package man
-  :defer t
-  :config (setq Man-width 80))
+(require 'prog-mode)
+(global-prettify-symbols-mode)
+(defun indicate-buffer-boundaries-left ()
+  (setq indicate-buffer-boundaries 'left))
+(add-hook 'prog-mode-hook #'indicate-buffer-boundaries-left)
 
-(use-package paren
-  :config (show-paren-mode))
+(require 'recentf)
+(add-to-list 'recentf-exclude "^/\\(?:ssh\\|su\\|sudo\\)?:")
 
-(use-package prog-mode
-  :config (global-prettify-symbols-mode)
-  (defun indicate-buffer-boundaries-left ()
-    (setq indicate-buffer-boundaries 'left))
-  (add-hook 'prog-mode-hook #'indicate-buffer-boundaries-left))
+(require 'savehist)
+(savehist-mode)
 
-(use-package recentf
-  :demand t
-  :config (add-to-list 'recentf-exclude "^/\\(?:ssh\\|su\\|sudo\\)?:"))
+(require 'saveplace)
+(save-place-mode)
 
-(use-package savehist
-  :config (savehist-mode))
+(require 'simple)
+(column-number-mode)
 
-(use-package saveplace
-  :when (version< "25" emacs-version)
-  :config (save-place-mode))
+(require 'smerge-mode)
+(when (>= emacs-major-version 27)
+  (set-face-attribute 'smerge-refined-changed nil :extend t)
+  (set-face-attribute 'smerge-refined-removed nil :extend t)
+  (set-face-attribute 'smerge-refined-added   nil :extend t))
 
-(use-package simple
-  :config (column-number-mode))
+(add-hook 'text-mode-hook #'indicate-buffer-boundaries-left)
 
-(use-package smerge-mode
-  :defer t
-  :config
-  (when (>= emacs-major-version 27)
-    (set-face-attribute 'smerge-refined-changed nil :extend t)
-    (set-face-attribute 'smerge-refined-removed nil :extend t)
-    (set-face-attribute 'smerge-refined-added   nil :extend t)))
+(require 'tramp)
+(add-to-list 'tramp-default-proxies-alist '(nil "\\`root\\'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist '("localhost" nil nil))
+(add-to-list 'tramp-default-proxies-alist
+             (list (regexp-quote (system-name)) nil nil))
+(setq vc-ignore-dir-regexp
+      (format "\\(%s\\)\\|\\(%s\\)"
+              vc-ignore-dir-regexp
+              tramp-file-name-regexp))
 
-(progn ;    `text-mode'
-  (add-hook 'text-mode-hook #'indicate-buffer-boundaries-left))
+(defvar spc-map (make-sparse-keymap))
 
-(use-package tramp
-  :defer t
-  :config
-  (add-to-list 'tramp-default-proxies-alist '(nil "\\`root\\'" "/ssh:%h:"))
-  (add-to-list 'tramp-default-proxies-alist '("localhost" nil nil))
-  (add-to-list 'tramp-default-proxies-alist
-               (list (regexp-quote (system-name)) nil nil))
-  (setq vc-ignore-dir-regexp
-        (format "\\(%s\\)\\|\\(%s\\)"
-                vc-ignore-dir-regexp
-                tramp-file-name-regexp)))
+(add-to-list 'load-path (expand-file-name "lib/evil/lib" user-emacs-directory))
+(setq evil-symbol-word-search t)
+(require 'evil)
+(evil-mode 1)
+(define-key evil-normal-state-map (kbd "SPC") spc-map)
 
-(use-package evil
-  :load-path "lib/evil/lib"
-  :config
-  (evil-mode 1))
+(require 'evil-magit)
 
-(use-package evil-magit)
+(require 'ivy)
+(require 'counsel)
+(require 'swiper)
+(ivy-mode 1)
+(counsel-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(define-key evil-motion-state-map (kbd "/") 'swiper)
+(define-key spc-map (kbd "/") 'counsel-rg)
+(define-key global-map (kbd "C-c C-r") 'ivy-resume)
 
-(use-package doom-themes
- :config
- (setq doom-themes-enable-bold t
-       doom-themes-enable-italic t)
- (load-theme 'doom-one t)
- (doom-themes-visual-bell-config)
- (doom-themes-org-config))
+(require 'doom-themes)
+(setq doom-themes-enable-bold t
+      doom-themes-enable-italic t)
+(load-theme 'doom-one t)
+(doom-themes-visual-bell-config)
+(doom-themes-org-config)
 
-(use-package airline-themes
- :config
- (load-theme 'airline-doom-one t))
+(require 'airline-themes)
+(load-theme 'airline-doom-one t)
 
-(progn ;     startup
-  (message "Loading %s...done (%.3fs)" user-init-file
-           (float-time (time-subtract (current-time)
-                                      before-user-init-time)))
-  (add-hook 'after-init-hook
-            (lambda ()
-              (message
-               "Loading %s...done (%.3fs) [after-init]" user-init-file
-               (float-time (time-subtract (current-time)
-                                          before-user-init-time))))
-            t))
+(message "Loading %s...done (%.3fs)" user-init-file
+         (float-time (time-subtract (current-time)
+                                    before-user-init-time)))
+(add-hook 'after-init-hook
+          (lambda ()
+            (message
+             "Loading %s...done (%.3fs) [after-init]" user-init-file
+             (float-time (time-subtract (current-time)
+                                        before-user-init-time))))
+          t)
 
-(progn ;     personalize
-  (let ((file (expand-file-name (concat (user-real-login-name) ".el")
-                                user-emacs-directory)))
-    (when (file-exists-p file)
-      (load file))))
+(let ((file (expand-file-name (concat (user-real-login-name) ".el")
+                              user-emacs-directory)))
+(when (file-exists-p file)
+  (load file)))
 
 ;; Local Variables:
 ;; indent-tabs-mode: nil
